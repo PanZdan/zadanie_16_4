34d97140437363a4a8e4c5002961b9b05a5c69f8
Pawel Schmidt <px.sxxx@gmail.com>
2018-02-02 22:33:34 +0100

    Added TodoForm

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,66 @@
-
-import React from 'react';
-import style from './App.css';
-import Title from '../components/Title.js';
-import TodoList from '../components/TodoList.js';
+import React from "react";
+import uuid from "uuid";
+import style from "./App.css";
+import Title from "../components/Title.js";
+import TodoList from "../components/TodoList.js";
+import TodoForm from "../components/TodoForm.js";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      data: [{
-        id: 1,
-            text: 'clean room'
-        }, {
-        id: 2,
-            text: 'wash the dishes'
-        }, {
-        id: 3,
-            text: 'feed my cat'
-        }]
+      value: "",
+      data: [
+        {
+          id: 1,
+          text: "clean room"
+        },
+        {
+          id: 2,
+          text: "wash the dishes"
+        },
+        {
+          id: 3,
+          text: "feed my cat"
+        }
+      ]
     };
   }
 
-  addTodo(val){
+  addTodo(val) {
     const todo = {
       text: val,
-      id: uuid.v4(),
+      id: uuid.v4()
     };
     const data = [...this.state.data, todo];
-    this.setState({data});
+    this.setState({ data });
   }
 
   removeTodo(id) {
     const remainder = this.state.data.filter(todo => todo.id !== id);
-    this.setState({data: remainder});
+    this.setState({ data: remainder });
+  }
+
+  onChangeHandle(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  onSubmitHandle(event) {
+    event.preventDefault();
+    this.addTodo(this.state.value);
   }
 
   render() {
     return (
       <div className={style.TodoApp}>
-        <Title todoName="ToDo" todoCount={this.state.data.length}/>
+        <Title todoName="ToDo" todoCount={this.state.data.length} />
+        <TodoForm
+          value={this.state.value}
+          onChangeHandle={this.onChangeHandle.bind(this)}
+          onSubmitHandle={this.onSubmitHandle.bind(this)}
+        />
         <TodoList todoList={this.state.data} />
       </div>
     );
